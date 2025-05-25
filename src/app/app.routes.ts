110e6072defb04a809eff4b58d2d18fb18fcf4cf
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-05-25 12:49:11 -0300

    Añadido ruta de escuela y estilo inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'product', loadChildren: () => import('./components/product/product.routes').then(m => m.productRoutes)},
+];
