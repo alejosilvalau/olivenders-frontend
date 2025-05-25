0374fdffbf11d0053b511b88e8de7d722192af73
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-05-25 13:29:21 -0300

    Actualizada configuración de .vscode para mejorar compatibilidad de tailwindcss@3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,19 +11,19 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app).to.be.ok;
   });
 
   it(`should have the 'olivenders-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('olivenders-frontend');
+    expect(app.title).equal('olivenders-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, olivenders-frontend');
+    expect(compiled.querySelector('h1')?.textContent).contain('Hello, olivenders-frontend');
   });
 });
