5e0c75c6381f035bdc2c444d7a64e86d29f8dcf7
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-05-25 11:08:16 -0300

    Agregada ruta de escuelas

---
+++ src/app/components/product/product.routes.ts
@@ -0,0 +1,6 @@
+import {Routes} from '@angular/router';
+import { SchoolComponent } from './school/school.component.js';
+
+export const productRoutes: Routes = [
+  {path: 'schools', component: SchoolComponent}
+]
