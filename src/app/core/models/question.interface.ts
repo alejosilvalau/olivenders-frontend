bc54c3cd8aa023abab58f2e37fd7f0e6cb75a125
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-07-14 18:01:38 -0300

    Terminada primera versión del servicio de entidad question

---
+++ src/app/core/models/question.interface.ts
@@ -0,0 +1,6 @@
+export interface Question {
+  id: string;
+  question: string;
+  created_at: Date;
+  options: string[];
+}
---
+++ src/app/core/services/question.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthToken } from '../../functions/authToken.function';
+import { environment } from '../../../environments/environment';
+import { Question } from '../models/question.interface.js';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class QuestionService {
+  private apiUrl = `${ environment.apiUrl }/questions`;
+  private authToken: AuthToken;
+
+  constructor(private http: HttpClient) {
+    this.authToken = new AuthToken();
+  }
+
+  findAll(): Observable<Question[]> {
+    const params = new HttpParams().set('page', '1').set('pageSize', '5');
+    return this.http.get<Question[]>(this.apiUrl, { params, headers: this.authToken.getAuthHeaders() });
+  }
+
+  findOne(id: string): Observable<Question> {
+    return this.http.get<Question>(`${ this.apiUrl }/${ id }`, { headers: this.authToken.getAuthHeaders() });
+  }
+
+  add(formData: FormData): Observable<Question> {
+    return this.http.post<Question>(this.apiUrl, formData, { headers: this.authToken.getAuthHeaders() });
+  }
+
+  update(id: string, formData: FormData): Observable<Question> {
+    return this.http.put<Question>(`${ this.apiUrl }/${ id }`, formData, { headers: this.authToken.getAuthHeaders() });
+  }
+
+  remove(id: string): Observable<Question> {
+    return this.http.delete<Question>(`${ this.apiUrl }/${ id }`, { headers: this.authToken.getAuthHeaders() });
+  }
+
+}
