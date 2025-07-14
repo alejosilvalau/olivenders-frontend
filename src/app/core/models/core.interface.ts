796ae09cc5236dc887354305b734b79488c1e1b2
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-07-14 17:49:53 -0300

    Terminada primera versión del servicio de entidad core

---
+++ src/app/core/models/core.interface.ts
@@ -0,0 +1,6 @@
+export interface Core {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+}
---
+++ src/app/core/services/core.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthToken } from '../../functions/authToken.function';
+import { environment } from '../../../environments/environment';
+import { Core } from '../models/core.interface.js';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoreService {
+  private apiUrl = `${ environment.apiUrl }/cores`;
+  private authToken: AuthToken;
+
+  constructor(private http: HttpClient) {
+    this.authToken = new AuthToken();
+  }
+
+  findAll(): Observable<Core[]> {
+    const params = new HttpParams().set('page', '1').set('pageSize', '5');
+    return this.http.get<Core[]>(this.apiUrl, { params, headers: this.authToken.getAuthHeaders() });
+  }
+
+  findOne(id: string): Observable<Core> {
+    return this.http.get<Core>(`${ this.apiUrl }/${ id }`, { headers: this.authToken.getAuthHeaders() });
+  }
+
+  findOneByName(name: string): Observable<Core> {
+    return this.http.get<Core>(`${ this.apiUrl }/name/${ name }`);
+  }
+
+  add(formData: FormData): Observable<Core> {
+    return this.http.post<Core>(this.apiUrl, formData, { headers: this.authToken.getAuthHeaders() });
+  }
+
+  update(id: string, formData: FormData): Observable<Core> {
+    return this.http.put<Core>(`${ this.apiUrl }/${ id }`, formData, { headers: this.authToken.getAuthHeaders() });
+  }
+
+  remove(id: string): Observable<Core> {
+    return this.http.delete<Core>(`${ this.apiUrl }/${ id }`, { headers: this.authToken.getAuthHeaders() });
+  }
+
+}
