df400ab279a6a766bd46e7f27f2f68bb0ceb0950
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-07-14 17:57:33 -0300

    Terminada primera versión del servicio de entidad wood

---
+++ src/app/core/models/wood.interface.ts
@@ -0,0 +1,7 @@
+export interface Wood {
+  id: string;
+  name: string;
+  binomial_name: string;
+  description: string;
+  price: number;
+}
---
+++ src/app/core/services/wood.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthToken } from '../../functions/authToken.function';
+import { environment } from '../../../environments/environment';
+import { Wood } from '../models/wood.interface.js';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WoodService {
+  private apiUrl = `${ environment.apiUrl }/woods`;
+  private authToken: AuthToken;
+
+  constructor(private http: HttpClient) {
+    this.authToken = new AuthToken();
+  }
+
+  findAll(): Observable<Wood[]> {
+    const params = new HttpParams().set('page', '1').set('pageSize', '5');
+    return this.http.get<Wood[]>(this.apiUrl, { params, headers: this.authToken.getAuthHeaders() });
+  }
+
+  findOne(id: string): Observable<Wood> {
+    return this.http.get<Wood>(`${ this.apiUrl }/${ id }`, { headers: this.authToken.getAuthHeaders() });
+  }
+
+  findOneByName(name: string): Observable<Wood> {
+    return this.http.get<Wood>(`${ this.apiUrl }/name/${ name }`);
+  }
+
+  add(formData: FormData): Observable<Wood> {
+    return this.http.post<Wood>(this.apiUrl, formData, { headers: this.authToken.getAuthHeaders() });
+  }
+
+  update(id: string, formData: FormData): Observable<Wood> {
+    return this.http.put<Wood>(`${ this.apiUrl }/${ id }`, formData, { headers: this.authToken.getAuthHeaders() });
+  }
+
+  remove(id: string): Observable<Wood> {
+    return this.http.delete<Wood>(`${ this.apiUrl }/${ id }`, { headers: this.authToken.getAuthHeaders() });
+  }
+
+}
