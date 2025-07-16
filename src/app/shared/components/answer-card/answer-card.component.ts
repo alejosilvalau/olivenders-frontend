1744b8671e438dce521a0a7b37864650bd97cd5a
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-07-16 08:46:52 -0300

    Creadad la primera versión del componente answer-card

---
+++ src/app/shared/components/answer-card/answer-card.component.ts
@@ -0,0 +1,25 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Answer } from '../../../core/models/answer.interface.js';
+import { Wand } from '../../../core/models/wand.interface.js';
+import { Wizard } from '../../../core/models/wizard.interface.js';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-answer-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './answer-card.component.html',
+  styleUrl: './answer-card.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AnswerCardComponent {
+  @Input() answer!: Answer;
+
+  get wand(): Wand | undefined {
+    return typeof this.answer.wand === 'object' ? this.answer.wand as Wand : undefined;
+  }
+
+  get wizard(): Wizard | undefined {
+    return typeof this.answer.wizard === 'object' ? this.answer.wizard as Wizard : undefined;
+  }
+}
