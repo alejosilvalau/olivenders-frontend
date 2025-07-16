36674cdaeeb03af50d9611d29c1a95891d9eae03
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-07-16 07:30:17 -0300

    Refactorizados los tipos de response y request del servicio de quiz

--- src/app/core/models/quiz.interface.ts
+++ src/app/core/models/quiz.interface.ts
@@ -1,6 +1,13 @@
+import { ApiResponse } from "./api-response.interface.js";
+import { Question } from "./question.interface.js";
+
 export interface Quiz {
   id: string;
   name: string;
-  created_at?: Date;
-  questions: string[];
+  created_at: Date;
+  questions: Question[] | string[];
 }
+
+export interface QuizRequest extends Omit<Quiz, 'id' | 'created_at'> { }
+
+export interface QuizResponse<T = Quiz> extends ApiResponse<T> { }
--- src/app/core/services/quiz.service.ts
+++ src/app/core/services/quiz.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthToken } from '../../functions/auth-token.function';
 import { environment } from '../../../environments/environment';
-import { Quiz } from '../models/quiz.interface.js';
+import { Quiz, QuizRequest, QuizResponse } from '../models/quiz.interface.js';
 
 @Injectable({
   providedIn: 'root',
@@ -16,24 +16,24 @@ export class QuizService {
     this.authToken = new AuthToken();
   }
 
-  findAll(page: number = 1, pageSize: number = 5): Observable<Quiz[]> {
+  findAll(page: number = 1, pageSize: number = 5): Observable<QuizResponse<Quiz[]>> {
     const params = new HttpParams().set('page', page).set('pageSize', pageSize);
-    return this.http.get<Quiz[]>(this.apiUrl, { params, headers: this.authToken.getAuthHeaders() });
+    return this.http.get<QuizResponse<Quiz[]>>(this.apiUrl, { params, headers: this.authToken.getAuthHeaders() });
   }
 
-  findOne(id: string): Observable<Quiz> {
-    return this.http.get<Quiz>(`${ this.apiUrl }/${ id }`, { headers: this.authToken.getAuthHeaders() });
+  findOne(id: string): Observable<QuizResponse> {
+    return this.http.get<QuizResponse>(`${ this.apiUrl }/${ id }`, { headers: this.authToken.getAuthHeaders() });
   }
 
-  add(formData: FormData): Observable<Quiz> {
-    return this.http.post<Quiz>(this.apiUrl, formData, { headers: this.authToken.getAuthHeaders() });
+  add(quizData: QuizRequest): Observable<QuizResponse> {
+    return this.http.post<QuizResponse>(this.apiUrl, quizData, { headers: this.authToken.getAuthHeaders() });
   }
 
-  update(id: string, formData: FormData): Observable<Quiz> {
-    return this.http.put<Quiz>(`${ this.apiUrl }/${ id }`, formData, { headers: this.authToken.getAuthHeaders() });
+  update(id: string, quizData: QuizRequest): Observable<QuizResponse> {
+    return this.http.put<QuizResponse>(`${ this.apiUrl }/${ id }`, quizData, { headers: this.authToken.getAuthHeaders() });
   }
 
-  remove(id: string): Observable<Quiz> {
-    return this.http.delete<Quiz>(`${ this.apiUrl }/${ id }`, { headers: this.authToken.getAuthHeaders() });
+  remove(id: string): Observable<QuizResponse> {
+    return this.http.delete<QuizResponse>(`${ this.apiUrl }/${ id }`, { headers: this.authToken.getAuthHeaders() });
   }
 }
