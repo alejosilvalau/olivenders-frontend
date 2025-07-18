61b0fc57d99081f5c92dd8641464c8e15ead8168
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-07-17 21:37:17 -0300

    Cambio mínimo en el componente de bottom-sheet

--- src/app/shared/components/bottom-sheet/bottom-sheet.component.ts
+++ src/app/shared/components/bottom-sheet/bottom-sheet.component.ts
@@ -8,7 +8,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './bottom-sheet.component.html',
-  styleUrls: ['./bottom-sheet.component.css']
+  styleUrl: './bottom-sheet.component.css'
 })
 export class BottomSheetComponent {
   constructor(
