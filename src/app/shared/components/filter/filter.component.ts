0c3f30467e7667a8241057fcbb5aa700b3ad2c13
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-07-17 21:55:34 -0300

    Refactorizado el componente de filtro

--- src/app/shared/components/filter/filter.component.ts
+++ src/app/shared/components/filter/filter.component.ts
@@ -1,73 +1,71 @@
-// import { Component, OnInit } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-// import { FormBuilder } from '@angular/forms';
-// import { BrandsService } from '../../../core/services/brands.service';
-// import { CategoriesService } from '../../../core/services/categories.service';
-// import { Brand } from '../../../core/models/brands.interfaces';
-// import { Category } from '../../../core/models/categories.interface';
-// import { FormsModule } from '@angular/forms';
-// import { EventEmitter } from '@angular/core';
-// import { Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { WoodService } from '../../../core/services/wood.service.js';
+import { CoreService } from '../../../core/services/core.service.js';
+import { Wood, WoodResponse } from '../../../core/models/wood.interface.js';
+import { Core, CoreResponse } from '../../../core/models/core.interface.js';
+import { FormsModule } from '@angular/forms';
+import { EventEmitter } from '@angular/core';
+import { Output } from '@angular/core';
 
-// @Component({
-//   selector: 'app-filter',
-//   standalone: true,
-//   imports: [CommonModule, ReactiveFormsModule, FormsModule],
-//   templateUrl: './filter.component.html',
-//   styleUrls: ['./filter.component.css'],
-// })
-// export class FilterComponent implements OnInit {
-//   @Output() filterChanged = new EventEmitter<any>();
+@Component({
+  selector: 'app-filter',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.css',
+})
+export class FilterComponent implements OnInit {
+  @Output() filterChanged = new EventEmitter<any>();
 
-//   filterForm: FormGroup;
-//   brands: Brand[] = [];
-//   categories: Category[] = [];
+  filterForm: FormGroup;
+  woods: WoodResponse<Wood[]> = { message: '' };
+  cores: CoreResponse<Core[]> = { message: '' };
 
-//   constructor(
-//     private fb: FormBuilder,
-//     private brandService: BrandsService,
-//     private categoriesService: CategoriesService
-//   ) {
-//     this.filterForm = this.fb.group({
-//       category: '',
-//       brand: '',
-//       priceDesde: 0,
-//       priceHasta: 0,
-//       kilometersDesde: 0,
-//       kilometersHasta: 0,
-//       isRentable: false,
-//       isBuyable: false,
-//     });
-//   }
+  constructor(
+    private fb: FormBuilder,
+    private woodService: WoodService,
+    private coreService: CoreService
+  ) {
+    this.filterForm = this.fb.group({
+      wood: '',
+      core: '',
+      minPrice: 0,
+      maxPrice: 0,
+      minlengthInches: 0,
+      maxLengthInches: 0,
+    });
+  }
 
-//   ngOnInit(): void {
-//     this.brandService.getAllBrand().subscribe({
-//       next: (brands: Brand[]) => {
-//         this.brands = brands;
-//       },
-//       error: (err) => {
-//         console.error('Error al obtener marcas:', err);
-//       },
-//     });
+  ngOnInit(): void {
+    this.woodService.findAll().subscribe({
+      next: (woods: WoodResponse<Wood[]>) => {
+        this.woods = woods;
+      },
+      error: (err) => {
+        console.error('Error fetching woods: ', err);
+      },
+    });
 
-//     this.categoriesService.getAllCategories().subscribe({
-//       next: (categories: Category[]) => {
-//         this.categories = categories;
-//       },
-//       error: (err) => {
-//         console.error('Error al obtener categorías:', err);
-//       },
-//     });
-//   }
+    this.coreService.findAll().subscribe({
+      next: (cores: CoreResponse<Core[]>) => {
+        this.cores = cores;
+      },
+      error: (err) => {
+        console.error('Error fetching cores: ', err);
+      },
+    });
+  }
 
-//   applyFilter(): void {
-//     const filterValues = this.filterForm.value;
-//     this.filterChanged.emit(filterValues);
-//   }
+  applyFilter(): void {
+    const filterValues = this.filterForm.value;
+    this.filterChanged.emit(filterValues);
+  }
 
-//   resetFilter(): void {
-//     this.filterForm.reset();
-//     this.filterChanged.emit({});
-//   }
-// }
+  resetFilter(): void {
+    this.filterForm.reset();
+    this.filterChanged.emit({});
+  }
+}
