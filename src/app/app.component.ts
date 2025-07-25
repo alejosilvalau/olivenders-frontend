7b76f5493a948b3c3619c1c05ef70e15d74ddaa3
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-07-24 21:25:40 -0300

    Cambiado nombres a classes del modal para respetar mejores prácticas de css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component.js';
-import { ParticlesBackgroundComponent } from './shared/components/particles-background/particles-background.component.js';
-// import { FooterComponent } from './shared/components/footer/footer.component.js';
+import { FooterComponent } from './shared/components/footer/footer.component.js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
