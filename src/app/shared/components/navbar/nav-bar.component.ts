c7243aff4547adeef6da461572cce15f8dc6a686
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-08-07 14:00:20 -0300

    Terminado el estilo de la navbar

--- src/app/shared/components/navbar/nav-bar.component.ts
+++ src/app/shared/components/navbar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service.js';
@@ -16,6 +16,7 @@ export class NavBarComponent {
   currentWizard: Wizard | null = null;
   wizardRole: WizardRole = WizardRole.User;
   isDropdownOpen = false;
+  isMobile = false;
   WizardRole = WizardRole;
 
   constructor(private authService: AuthService) { }
@@ -29,6 +30,25 @@ export class NavBarComponent {
       this.currentWizard = wizard;
       this.wizardRole = wizard?.role || null;
     });
+
+    this.checkMobile();
+    window.addEventListener('resize', this.checkMobile.bind(this));
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('resize', this.checkMobile.bind(this));
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (
+      this.isDropdownOpen &&
+      !target.closest('#dropdown') &&
+      !target.closest('#dropdownDefaultButton')
+    ) {
+      this.isDropdownOpen = false;
+    }
   }
 
   logout(): void {
@@ -39,5 +59,9 @@ export class NavBarComponent {
 
   toggleDropdown(): void {
     this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  checkMobile() {
+    this.isMobile = window.innerWidth <= 767;
   }
 }
