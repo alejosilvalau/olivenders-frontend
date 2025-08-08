5159ab42e725fef7160490333ad1330777c14333
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-08-08 20:22:47 -0300

    Terminado de corregir el componente de gestión de escuelas

--- src/app/components/product/school-management/school-management.component.ts
+++ src/app/components/product/school-management/school-management.component.ts
@@ -6,7 +6,7 @@ import { School, SchoolResponse } from '../../../core/models/school.interface';
 import { Observable } from 'rxjs';
 import { SearcherComponent } from '../../../shared/components/searcher/searcher.component';
 import { AlertComponent, AlertType } from '../../../shared/components/alert/alert.component';
-import { DataTableComponent } from '../../../shared/components/data-table/data-table.component.js';
+import { DataTableComponent, DataTableFormat } from '../../../shared/components/data-table/data-table.component.js';
 import { AddButtonComponent } from '../../../shared/components/add-button/add-button.component.js';
 import { ModalComponent } from '../../../shared/components/modal/modal.component.js';
 import { PaginationComponent } from '../../../shared/components/pagination/pagination.component.js';
@@ -27,6 +27,8 @@ export class SchoolManagementComponent implements OnInit {
   totalSchools = 0;
   currentPage = 1;
   pageSize = 10;
+
+  DataTableFormat = DataTableFormat;
 
   @ViewChild(AlertComponent) alertComponent!: AlertComponent
 
@@ -110,14 +112,15 @@ export class SchoolManagementComponent implements OnInit {
         next: (res: SchoolResponse) => {
           this.alertComponent.showAlert(res.message, AlertType.Success);
           this.findAllSchools();
+          this.schoolForm.reset();
         },
         error: (err: any) => {
           this.alertComponent.showAlert(err.error.message, AlertType.Error);
+          this.schoolForm.reset();
         }
       });
-      this.schoolForm.reset();
     } else {
-      this.alertComponent.showAlert('Please complete all required fields.', AlertType.Error);
+      this.alertComponent.showAlert('Please complete all required fields', AlertType.Error);
     }
   }
 
@@ -128,12 +131,13 @@ export class SchoolManagementComponent implements OnInit {
         next: (response: SchoolResponse) => {
           this.alertComponent.showAlert(response.message, AlertType.Success);
           this.findAllSchools();
+          this.schoolForm.reset();
         },
         error: (err: any) => {
           this.alertComponent.showAlert(err.error.message, AlertType.Error);
+          this.schoolForm.reset();
         }
       });
-      this.schoolForm.reset();
     }
   }
 
@@ -143,12 +147,13 @@ export class SchoolManagementComponent implements OnInit {
         next: (response: SchoolResponse) => {
           this.alertComponent.showAlert(response.message, AlertType.Success);
           this.findAllSchools();
+          this.schoolForm.reset();
         },
         error: (err: any) => {
           this.alertComponent.showAlert(err.error.message, AlertType.Error);
+          this.schoolForm.reset();
         }
       });
-      this.schoolForm.reset();
     }
   }
 }
