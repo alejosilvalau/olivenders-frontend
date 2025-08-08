bd2d8ff02af83195a4f495337fe5d40cd24217b3
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-08-08 10:53:57 -0300

    Terminado el estilo de las alertas tipo pop up

--- src/app/functions/alert.function.ts
+++ src/app/functions/alert.function.ts
@@ -3,8 +3,13 @@ import Swal from 'sweetalert2';
 
 export function alertMethod(title: string, message: string, type: any) {
     Swal.fire({
-        title: title,
-        text: message,
-        icon: type,
-      })
+      title: title,
+      text: message,
+      icon: type,
+      customClass: {
+        popup: 'custom-swal-popup',
+        title: 'custom-swal-title',
+        confirmButton: 'custom-swal-confirm',
+      }
+    })
 }
